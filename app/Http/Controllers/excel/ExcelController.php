ce82b354e9eb7b32ad90ca1d185d76dee5350432
weiyalin <727299708@qq.com>
2017-09-14 08:31:44 +0800

    '报名截止'

--- app/Http/Controllers/excel/ExcelController.php
+++ app/Http/Controllers/excel/ExcelController.php
@@ -16,20 +16,45 @@ class ExcelController extends Controller
 {
     public function export(){
         $cellData = [
-            ['序号','姓名','班级','学号','电话','报名时间'],
+            ['序号','姓名','班级','学号','电话','方向','报名时间'],
         ];
-        $Data = DB::table("student")->get();
+        $Data = DB::table("student")->where('radio',1)->get();
         foreach ($Data as $student ){
+            $dir = '开发';
             $cellData[] = [
                 $student->id,
                 $student->name,
                 $student->grade,
                 $student->student_id,
                 $student->phone_num,
+                $dir,
                 date('Y-m-d', $student->create_time/1000),
             ];
         }
-        Excel::create('新生报名信息',function($excel) use ($cellData){
+        Excel::create('开发新生报名信息',function($excel) use ($cellData){
+            $excel->sheet('score', function($sheet) use ($cellData){
+                $sheet->rows($cellData);
+            });
+        })->export('xls');
+    }
+    public function export2(){
+        $cellData = [
+            ['序号','姓名','班级','学号','电话','方向','报名时间'],
+        ];
+        $Data = DB::table("student")->where('radio',2)->get();
+        foreach ($Data as $student ){
+            $dir = '美工';
+            $cellData[] = [
+                $student->id,
+                $student->name,
+                $student->grade,
+                $student->student_id,
+                $student->phone_num,
+                $dir,
+                date('Y-m-d', $student->create_time/1000),
+            ];
+        }
+        Excel::create('美工新生报名信息',function($excel) use ($cellData){
             $excel->sheet('score', function($sheet) use ($cellData){
                 $sheet->rows($cellData);
             });
--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,5 @@ Route::get('/', function () {
 Route::post('/sign','student\StudentController@sign');
 Route::post('/search','student\StudentController@search');
 Route::get('/export','excel\ExcelController@export');
+Route::get('/export2','excel\ExcelController@export2');
 Route::post('/reset','student\StudentController@reset');
